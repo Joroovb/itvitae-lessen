23d8fdd09e43a8716dd5f6c85525580316594a48
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-15 11:15:32 +0200

    Les 2: Opdracht signed integers

--- Les 2/scratchpad/src/Main.java
+++ Les 2/scratchpad/src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
+
     public static void main(String[] args) {
-        int i = 128;
-        byte b = (byte) i;
-        System.out.println(b);
+        byte aantalUren = 5;
+        byte aantalDagen = 6;
+        short minutenInUur = minutenInUur(aantalUren);
+        short minutenInAantalDagen = (short) (minutenInUur((short)(aantalDagen * 24)));
+
+
+        System.out.println(minutenInUur);
+    }
+
+    static short minutenInUur(short aantalUren) {
+        return (short) (aantalUren * 60);
     }
 }
