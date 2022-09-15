445db130df50f0ac5bbc8f9f5dc04affb57d9a0e
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-15 14:09:09 +0200

    Les 2: Operators

--- Les 2/scratchpad/src/Main.java
+++ Les 2/scratchpad/src/Main.java
@@ -3,20 +3,43 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Voer een tekenreeks in: ");
-        String invoer = scan.nextLine();
-        invoer = invoer.toUpperCase();
+        int getal = 1; // is
+        getal += 1;
+        getal = getal + 1; // hetzelfde als regel erboven
+        getal -= 1;
+        getal /= 1; // getal = getal / 1;
 
-        String output = "";
-        for (int i = 0; i < invoer.length(); i++) {
-            char temp =  (char) (invoer.charAt(i) + 3);
-            if (temp > 'Z') {
-                temp = (char) (temp - 26);
-            }
-            output += temp;
-        }
+        getal = 1 + 1; // optellen
+        getal = 1 - 1; // aftrekken
+        getal = 1 * 2; // keer
+        getal = 2 / 2; // delen
+        getal = 10 % 3; // modulus
 
-        System.out.println(output);
+        getal = 10;
+//        getal++; // postfix increment
+//        ++getal; // prefix increment
+
+        System.out.println(getal++);
+        System.out.println(getal++);
+        System.out.println(++getal);
+
+        boolean b = getal > 10; // groter
+        b = getal < 10; // kleiner
+        b = getal >= 10; // groter dan of gelijk aan
+        b = getal <= 10; // kleiner dan of gelijk aan
+        b = getal == 10; // gelijk aan
+        b = getal != 10; // niet gelijk aan
+
+        System.out.println(!b); // boolean flippen
+        b = getal <  10 && getal < 6; // and operator short circuited
+        b = getal <  10 & getal < 6; // and operator
+        b = getal > 12 || getal++ < 10; // or operator short circuited
+        b = getal > 12 | getal < 10; // or operator
+
+        System.out.println(getal);
+        System.out.println(b);
+
+
+
     }
 }
