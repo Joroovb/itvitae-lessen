921067de5a1f8a2021544d07db528ef43074ae44
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-15 12:26:46 +0200

    Les 2: Float opdracht 2 snelheid bereken

--- Les 2/scratchpad/src/Main.java
+++ Les 2/scratchpad/src/Main.java
@@ -1,20 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-//        float f = 5.4; // Compileert niet, default is double
-        float fff = 5.4f;
-        float f = CToF(-40);
-        System.out.println("f: " + f);
-        float c = FToC(f);
-        System.out.println("c: " + c);
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Voer het aantal meters in: ");
+        int afstand = scan.nextInt();
+        System.out.print("Voer het aantal uren in: ");
+        int uren = scan.nextInt();
+        System.out.print("Voer het aantal minuten in: ");
+        int minuten = scan.nextInt();
+        System.out.print("Voer het aantal seconden in: ");
+        int seconden = scan.nextInt();
+
+        minuten = minuten + urenNaarMin(uren);
+        seconden = seconden + minNaarSec(minuten);
+        float mps = meterPerSeconde(afstand, seconden);
+        float kpu = kmPerUur(mps);
+        float mpu = mijlPerUur(kpu);
+        System.out.println("Uw snelheid in m/s: " + mps);
+        System.out.println("Uw snelheid in km/u: " + kpu);
+        System.out.println("Uw snelheid in m/u: " + mpu);
+
 
     }
 
-    static float FToC(float temp) {
-        return (temp - 32) / 1.8f;
+    static short urenNaarMin(int uren) {
+        return (short) (uren * 60);
     }
 
-    static float CToF(float temp) {
-        return temp * 1.8f + 32;
+    static int minNaarSec(int min) {
+        return min * 60;
+    }
+
+    static float meterPerSeconde(int meter, int seconden) {
+        return (float) meter / seconden;
+    }
+
+    static float kmPerUur(float mps) {
+        return mps * 3.6f;
+    }
+
+    static float mijlPerUur(float kpu) {
+        return kpu / 1.609f;
     }
 }
