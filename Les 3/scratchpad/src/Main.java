de2ef784fd3eae0428993d7284393180d653ee29
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-19 11:57:31 +0200

    Les 3: Opdrachten tot de macht & oneven

--- Les 3/scratchpad/src/Main.java
+++ Les 3/scratchpad/src/Main.java
@@ -2,23 +2,57 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Test t = new Test();
-        t.method1(5);
+        System.out.println(exponent(3, 3));
+        System.out.println(exponent2(3, 3));
+        System.out.println(exponent3(3, 3));
+        System.out.println(OddEvenTest.isOdd(4));
+        System.out.println(OddEvenTest.isOdd2(4));
+        System.out.println(OddEvenTest.isOdd3(4));
     }
 
+    public static int exponent(int base, int exp) {
+        if (exp >= 0) {
+            int result = 1;
+            for (int i = 0; i < exp; i++) {
+                result *= base;
+            }
+            return result;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int exponent2(int base, int exp) {
+        return (int) Math.pow(base, exp);
+    }
+
+    public static int exponent3(int base, int exp) {
+        if (exp == 0) {
+            return 1;
+        } else if (exp < 0) {
+            return base / exponent3(base, exp + 1);
+        } else {
+            return base * exponent3(base, exp - 1);
+        }
+    }
 }
-class Test {
-    static int x = 11;
-    static private int y = 33;
 
-    public void method1(int x) {
-        Test t = new Test();
-        this.x = 22;
-        y = 44;
+class OddEvenTest {
+    static boolean isOdd(int invoer) {
+        if (invoer % 2 != 0) {
+            return true;
+        }
+        return false;
+    }
 
-        System.out.println(Test.x);
-        System.out.println(t.x);
-        System.out.println(t.y);
-        System.out.println(y);
+    static boolean isOdd2(int invoer) {
+        return invoer % 2 != 0;
+    }
+
+    static boolean isOdd3(int invoer) {
+        if (((invoer & 1)) == 0) {
+            return false;
+        }
+        return true;
     }
 }
