bdca17889a0464b3a1901749bafb7aca3bc03347
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-19 09:59:35 +0200

    Les 3: Init

--- Les 2/scratchpad/src/Main.java
+++ Les 2/scratchpad/src/Main.java
@@ -3,29 +3,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //256 128 64 32 16 8 4 2 1
-        //                       0 => 0
-        //                       1 => 1
-        //                     1 0 => 2
-        //                     1 1 => 3
-        // 0    0  0  1  0 0 1 0 1 => 37
-        // 0    1  0  0  1 1 0 1 0 => 154
-        // 0    0  0  0  0 0 0 0 0  => 0
-        // 0    1  0  1  1 1 1 1 1 => 191
+        Scanner input = new Scanner(System.in);
+        boolean positief = false;
+        int getal;
 
-        // 4 => 0100
-        // 1 => 0001
-        // =>   0101 uitkomst van 1 | 4
-        // ^
-        // 5 => 0101
-        // 1 => 0001
-        // =>   0100 => 4
-
-//        int getal = 100;
-//        System.out.println(getal);
-
-
-
+        while(!positief) {
+            getal = input.nextInt();
+            if (getal <= 0) {
+                System.out.println("Voer een positief getal in");
+                continue;
+            }
+            positief = true;
+        }
 
     }
 }
