5607ca542292da28b2baaa54b3a138663b3954ae
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-19 13:18:10 +0200

    Les 3: Constructors en overloads

--- Les 3/scratchpad/src/Main.java
+++ Les 3/scratchpad/src/Main.java
@@ -2,24 +2,29 @@ public class Main {
 
 
     public static void main(String[] args) {
-        max(1, 2.0);
-        max(1.0, 2);
-//        max(1, 2); // Ambigue
+        Persoon p = new Persoon("Joris", 29);
+        Persoon baby = new Persoon("Jan");
+    }
+}
+
+class Persoon {
+    String naam;
+    int leeftijd; // default waarde = 0
+
+    public Persoon(String naam, int leeftijd) {
+        this(naam);
+        System.out.println("2 argumenten");
+        this.leeftijd = leeftijd;
     }
 
-   static int max(int i, double b) {
-       System.out.println("Int");
-       return 0;
-   }
+    public Persoon(String naam) {
+        this();
+        System.out.println("1 argument");
+        this.naam = naam;
+    }
 
-   static double max(double i, int m) {
-       System.out.println("Double");
-       return 0.0;
-   }
+    public Persoon() {
 
-   static void max(int i, int m) {
-       System.out.println("Ik print iets");
-   }
-
+    }
 }
 
