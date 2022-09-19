a2f9e5fdbc006f71aefc9fdc11c56f2f3dadc5eb
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-19 10:49:16 +0200

    Les 3: object referenties

--- Les 3/scratchpad/src/Main.java
+++ Les 3/scratchpad/src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
+    int voorbeeld = 50;
+
     public static void main(String[] args) {
-        System.out.println(tekst);
-        String tekst = "Dit werkt niet";
+       new Main().testmethode();
+       Main m = new Main();
+        System.out.println(new Main());
+        System.out.println(new Main());
     }
+
+    void testmethode() {
+//        Main m = new Main();
+        int voorbeeld = 60; // shadowing
+
+        if (voorbeeld < 100) {
+            voorbeeld = 120;
+        }
+
+        System.out.println(voorbeeld);
+        System.out.println(this.voorbeeld);
+    }
+
 }
