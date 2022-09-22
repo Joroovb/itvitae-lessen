12cc9e3145608e5f79d76154ae24676896075dc3
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-22 10:27:33 +0200

    Les 4: string methodes

--- Les 4/scratchpad/src/Main.java
+++ Les 4/scratchpad/src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        
+        String naam = "\t        Joris          \n";
+        System.out.println(naam.trim());
+
+        String letters = "ABCAB";
+        System.out.println(letters.replace("CA", "12"));
+        System.out.println("".length());
+        System.out.println(letters.startsWith("A", 3)); // begin met checken vanaf index 3
+//        String naam2 = "KLaas";
+//        char letter = naam.charAt(2);
+//
+//        System.out.println(letter);
+//
+//        String naamStuk = naam2.substring(2, 5);
+//        String naamStuk2 = naam2.substring(2, naam2.length());
+//        String naamStuk3 = naam2.substring(2); // komt op hetzelfde neer als erboven
+//        System.out.println(naamStuk3);
+//
+//        System.out.println(naamStuk);
+//        int i = naam2.indexOf('a');
+//
+//        System.out.println(i);
+//
+//        int length = naam.length();
+//        System.out.println(letter);
+//        System.out.println(length);
+
     }
 }
