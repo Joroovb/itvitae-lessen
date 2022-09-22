823ddbfa7baba7d25d1df1a5a50ceb4fa29c5818
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-22 11:30:56 +0200

    Les 4: Stringbuilder

--- Les 4/scratchpad/src/Main.java
+++ Les 4/scratchpad/src/Main.java
@@ -1,42 +1,16 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Bon");
+        sb.insert(2, 'r');
+        System.out.println(sb);
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Geef een string op: ");
-        String input = in.nextLine();
-        String output = "";
+        StringBuilder sb1 = new StringBuilder("123");
+        char[] naam = {'J', 'a', 'v', 'a'};
+        sb1.insert(2, naam, 1, 3);
+        System.out.println(sb1);
 
-        for (int i = input.length() - 1; i >= 0; i--) {
-            output += input.charAt(i);
-        }
-
-        String naam = "Joris";
-        naam.toLowerCase(); // veranderd origineel niet
-        System.out.println(naam);
-
-        String naam2 = "Joris";
-        naam2 = naam2.toLowerCase();
-        System.out.println(naam2);
-
-        System.out.println(output);
-
-        if (input.equalsIgnoreCase(output)) {
-            System.out.println("Palindrome");
-        }
-
-        String tekst = "Hallo; Hallo;";
-        String tekst2 = "Hallo;";
-
-        String tekst3 = "Hallo; Hallo;";
-
-
-
-        if (tekst3 == tekst) {
-            System.out.println("Gelijk");
-        } else {
-            System.out.println("Niet gelijk");
-        }
+        StringBuilder sb2 = new StringBuilder("Joris");
+        sb2.delete(1, 3); // exclusief end index
+        System.out.println(sb2);
     }
 }
