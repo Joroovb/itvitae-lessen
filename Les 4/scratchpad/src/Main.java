89f45f7c4036a16ed90860d3f7cee1f13e9daadc
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-22 13:41:47 +0200

    Les 4: Opdracht tweede manier

--- Les 4/scratchpad/src/Main.java
+++ Les 4/scratchpad/src/Main.java
@@ -2,37 +2,62 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
+    Scanner in = new Scanner(System.in);
+    Scanner in2 = new Scanner(System.in);
+
     public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
+        Main m = new Main();
         System.out.print("Voer het aantal items in: ");
+        int getal = m.inputLengte();
+        System.out.println("U heeft " + getal + " getallen gekozen.");
+        System.out.println("Voer uw getallen in: ");
+        System.out.print(Arrays.toString(m.arrayValues(getal)));
 
-        int NUM_ITEMS = input.nextInt();
-        System.out.print("Voer de waarde van alle items in gescheiden door spaties: ");
+//       Scanner input = new Scanner(System.in);
+//        System.out.print("Voer het aantal items in: ");
+//
+//        int NUM_ITEMS = input.nextInt();
+//        System.out.print("Voer de waarde van alle items in gescheiden door spaties: ");
+//
+//        int[] items = new int[NUM_ITEMS];
+//
+//        for (int i = 0; i < NUM_ITEMS; i++) {
+//            items[i] = input.nextInt();
+//        }
+//
+//        System.out.println(Arrays.toString(items));
+//
+//
+//        System.out.print("De waarden zijn [");
+//
+//
+//        for (int i = 0; i < NUM_ITEMS; i++) {
+//            System.out.print(items[i]);
+//            if (i < NUM_ITEMS -1) {
+//                System.out.print(", ");
+//            }
+//        }
+//
+//        System.out.println("]");
+//
+//        for (int item : items) {
+//            System.out.println("*".repeat(item));
+//        }
+    }
 
-        int[] items = new int[NUM_ITEMS];
-
-        for (int i = 0; i < NUM_ITEMS; i++) {
-            items[i] = input.nextInt();
+    private int[] arrayValues(int getal) {
+        String rawItems = in2.nextLine();
+        String[] arrayItems = rawItems.split(" ");
+        int[] items = new int[arrayItems.length];
+        for (int i = 0; i < arrayItems.length; i++) {
+            items[i] = Integer.parseInt(arrayItems[i]);
         }
+        return items;
+    }
 
-        System.out.println(Arrays.toString(items));
-
-
-        System.out.print("De waarden zijn [");
-
-
-        for (int i = 0; i < NUM_ITEMS; i++) {
-            System.out.print(items[i]);
-            if (i < NUM_ITEMS -1) {
-                System.out.print(", ");
-            }
-        }
-
-        System.out.println("]");
-
-        for (int item : items) {
-            System.out.println("*".repeat(item));
-        }
-
+    private int inputLengte() {
+        int NUM_ITEMS = in.nextInt();
+        return NUM_ITEMS;
     }
 }
