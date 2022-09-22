2bd0916083a4617db7708d03aabff350704bf41a
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-22 14:12:18 +0200

    Les 4: Arraylists

--- Les 4/scratchpad/src/Main.java
+++ Les 4/scratchpad/src/Main.java
@@ -1,23 +1,44 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> namen = new ArrayList<>();
+        String[] namen2 = new String[4];
+        namen.add("Joris");
+        namen.add("Melinoe");
+        namen.add(1,"Salem");
 
-        
-        int[][] getallen = {{1, 2, 3}, {6, 7 , 8}, {1, 2}};
+        System.out.println(namen);
 
-//        int[][][] drie = { {{}, {}}, {}, {} };
-
-        System.out.println(getallen[2][0]);
-
-
-
-        for (int i = 0; i < getallen.length; i++) {
-            for (int ii = 0; ii < getallen[i].length; ii++ ) {
-//                System.out.println(getallen[i][ii]);
-            }
-//            System.out.println(getallen[i]);
+        for (String naam : namen) {
+            System.out.println(naam);
         }
+
+        for (int i = 0; i < namen.size(); i++) {
+            System.out.println(namen.get(i));
+            if (namen.get(i).equals("Salem")) {
+                namen.set(i, "Chili");
+            }
+        }
+        System.out.println(namen);
+        System.out.println(namen.indexOf("Salem"));
+
+        namen.remove(0);
+        System.out.println(namen);
+        namen.remove("Chili");
+        System.out.println(namen);
+
+
+        ArrayList<Integer> getallen = new ArrayList<>();
+        getallen.add(1);
+        getallen.add(2);
+        getallen.add(3);
+        System.out.println(getallen);
+
+        getallen.remove(Integer.valueOf(2));
+        System.out.println(getallen);
     }
 }
