b400612861ed93af20aa237d7f04e10189fab5d4
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-26 13:09:20 +0200

    Les 5: Enhanced for loop

--- Les 5/scratchpad/src/Main.java
+++ Les 5/scratchpad/src/Main.java
@@ -4,55 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String[] namen = {"Joris", "Melinoe", "Salem"};
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Tafelmaker: Geef de grootte aan: ");
-        int grootte = input.nextInt();
-
-        System.out.print("* | ");
-        for (int i = 1; i <= grootte; i++) {
-            System.out.print(" " + (i < 10 ? " " + i : i));
+        for (int i = 0; i < namen.length; i++) {
+            System.out.println("Hier woont: " + namen[i]);
         }
 
-        System.out.println();
-
-        System.out.println("---".repeat(grootte + 1));
-
-        for (int i = 1; i <= grootte; i++) {
-            System.out.print((i < 10 ? " " + i : i)+ "|");
-            for (int j = 1; j <= grootte; j++) {
-                int ij = i * j;
-                System.out.print(" " + (ij < 10 ? " " + ij : ij));
-            }
-            System.out.println();
+        for (String naam : namen) {
+            System.out.println("Hier woont: " + naam);
         }
-
-
-
-//        int[] getallen = {1, 2, 3, 4, 5, 6};
-//        int som = 0;
-//        //  init           ; eval     ;  stap
-//        for (int index = 0; index < 10; index++) {
-////            int som = 0;
-//            som += index;
-////            System.out.println(som);
-//
-//        }
-////        System.out.println(som);
-//
-//        for (int index = 0; index < getallen.length; index++) {
-////            System.out.println(getallen[index]);
-//        }
-//
-//        outer: for (int uren = 1; uren <= 6; uren++) {
-//            for (int min = 0; min < 60; min++) {
-//                if (uren == 3 && min == 45) {
-//                    System.out.println(uren + " : " + min);
-//                    break outer;
-//                }
-//                System.out.println(uren + " : " + min);
-//
-//            }
-//        }
     }
 }
