00a6415b96b8e8c38b1c004df4fadbd2fe371121
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-26 11:24:58 +0200

    Les 5: Opdracht 1

--- Les 5/scratchpad/src/Main.java
+++ Les 5/scratchpad/src/Main.java
@@ -1,14 +1,43 @@
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Month;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String okee;
-        int leeftijd = 17;
-//              conditie        Waarde als waar  Waarde als niet waar
-        okee = leeftijd >= 18 ? "Okee" :        "Niet okee";
+        Scanner input = new Scanner(System.in);
+        char tekstCijfer = '-';
+        System.out.print("Vul het cijfer in: ");
+        int cijfer = input.nextInt();
+        tekstCijfer = cijferGetal(cijfer);
+        tekstCijfer = cijferGetal2(cijfer);
 
-        System.out.println(okee);
+        System.out.println("Je hebt gehaald: " + tekstCijfer);
+    }
+
+    private static char cijferGetal2(int cijfer) {
+        int[] scores = {25, 45, 50, 60, 80};
+        char[] letters = {'F', 'E', 'D', 'C', 'B'};
+        for (int i = 0; i < scores.length; i++) {
+            if (cijfer < scores[i]) {
+                return letters[i];
+            }
+        }
+        return 'A';
+    }
+
+    private static char cijferGetal(int cijfer) {
+        if (cijfer < 25) {
+            return 'F';
+        } else if (cijfer < 45) {
+            return 'E';
+        } else if (cijfer < 50) {
+            return 'D';
+        } else if (cijfer < 60) {
+            return 'C';
+        } else if (cijfer < 80) {
+            return 'B';
+        } else {
+            return 'A';
+        }
     }
 }
