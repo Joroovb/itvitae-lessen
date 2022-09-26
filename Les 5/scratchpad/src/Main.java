79a280e498bb897979cd960c662ab52d0abd376f
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-26 10:37:07 +0200

    Les 5: If statements

--- Les 5/scratchpad/src/Main.java
+++ Les 5/scratchpad/src/Main.java
@@ -4,12 +4,41 @@ import java.time.Month;
 
 public class Main {
     public static void main(String[] args) {
-//        LocalDate d = LocalDate.of(2022, Month.FEBRUARY, 29);
-//        System.out.println(d);
+        int leeftijd = 101;
+//
+//        if (leeftijd >= 18) {
+//            System.out.println("Je bent welkom");
+//            System.out.println("Je bent welkom");
+//        } else if (leeftijd <= 16)
+//            System.out.println("Ga naar huis");
+//        else if (leeftijd < 15)
+//            System.out.println("We gaan je ouders bellen");
+//        else
+//            System.out.println("Jij bent te jong");
 
-        LocalTime t = LocalTime.of(12, 12, 12, 12);
-        LocalTime tt = LocalTime.now();
+        // guard clause
+        boolean ouderDanHonderd = leeftijd > 100;
 
-        System.out.println(tt);
+        if (ouderDanHonderd) {
+            System.out.println("Ik bel de burgemeester");
+            return;
+        }
+
+        // ingelogd
+
+        // authorizatie
+
+
+
+        if (leeftijd >= 18) {
+            if (leeftijd > 45) {
+                if (leeftijd > 100) {
+                    System.out.println("Ik bel de burgemeester");
+                }
+                System.out.println("Ga naar huis");
+            } else {
+                System.out.println("kom binnen");
+            }
+        }
     }
 }
