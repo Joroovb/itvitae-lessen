ceaf14eb4f11eb2a595ab8ce780a8d6f3a36989c
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-26 10:18:22 +0200

    Les 5: Dates

--- Les 5/scratchpad/src/Main.java
+++ Les 5/scratchpad/src/Main.java
@@ -1,8 +1,15 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Month;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        LocalDate d = LocalDate.of(2022, Month.FEBRUARY, 29);
+//        System.out.println(d);
+
+        LocalTime t = LocalTime.of(12, 12, 12, 12);
+        LocalTime tt = LocalTime.now();
+
+        System.out.println(tt);
     }
 }
