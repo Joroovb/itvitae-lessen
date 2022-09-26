52f7ed67e05f27c7454348d334b61e43e05d7cd6
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-26 13:28:36 +0200

    Les 5: while & do while

--- Les 5/scratchpad/src/Main.java
+++ Les 5/scratchpad/src/Main.java
@@ -4,14 +4,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] namen = {"Joris", "Melinoe", "Salem"};
 
-        for (int i = 0; i < namen.length; i++) {
-            System.out.println("Hier woont: " + namen[i]);
+        // while
+        int i = 0;
+        int[] list = new int[7];
+
+        while (i < list.length) {
+            list[i] = i +2;
+            i++;
         }
 
-        for (String naam : namen) {
-            System.out.println("Hier woont: " + naam);
+        //do while
+        i = 0;
+        list = new int[7];
+
+        do {
+            list[i] = i + 2;
+            i++;
+        } while (i < list.length);
+
+        System.out.println(list[6]);
+
+
+        System.out.println(list[2]);
+
+        int count = 0;
+
+        do {
+
+        } while (count < 100);
+
+        while (count < 100) {
+            System.out.println(count);
+            count += 2;
         }
     }
 }
