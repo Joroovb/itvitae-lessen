43256ac87f982f738a081b493c1363f66cc2f0a1
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-26 11:15:45 +0200

    Les 5: Ternary operator

--- Les 5/scratchpad/src/Main.java
+++ Les 5/scratchpad/src/Main.java
@@ -4,41 +4,11 @@ import java.time.Month;
 
 public class Main {
     public static void main(String[] args) {
-        int leeftijd = 101;
-//
-//        if (leeftijd >= 18) {
-//            System.out.println("Je bent welkom");
-//            System.out.println("Je bent welkom");
-//        } else if (leeftijd <= 16)
-//            System.out.println("Ga naar huis");
-//        else if (leeftijd < 15)
-//            System.out.println("We gaan je ouders bellen");
-//        else
-//            System.out.println("Jij bent te jong");
+        String okee;
+        int leeftijd = 17;
+//              conditie        Waarde als waar  Waarde als niet waar
+        okee = leeftijd >= 18 ? "Okee" :        "Niet okee";
 
-        // guard clause
-        boolean ouderDanHonderd = leeftijd > 100;
-
-        if (ouderDanHonderd) {
-            System.out.println("Ik bel de burgemeester");
-            return;
-        }
-
-        // ingelogd
-
-        // authorizatie
-
-
-
-        if (leeftijd >= 18) {
-            if (leeftijd > 45) {
-                if (leeftijd > 100) {
-                    System.out.println("Ik bel de burgemeester");
-                }
-                System.out.println("Ga naar huis");
-            } else {
-                System.out.println("kom binnen");
-            }
-        }
+        System.out.println(okee);
     }
 }
