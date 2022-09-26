f668eafe0d8ff734a23f522514a8b5070641a88e
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-26 11:48:30 +0200

    Les 5: Switch opdracht

--- Les 5/scratchpad/src/Main.java
+++ Les 5/scratchpad/src/Main.java
@@ -4,19 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
-       int getal = input.nextInt();
-        System.out.print("Fibonacci: " + fibonacci2(getal));
+        Scanner input = new Scanner(System.in);
+        String browser = input.nextLine();
+
+        switch(browser) {
+            case "Edge":
+                System.out.println("Je gebruikt Edge");
+                break;
+            case "Chrome":
+            case "Opera":
+            case "Firefox":
+                System.out.println("Je gebruikt een moderne browser");
+                break;
+            case "IE":
+                System.out.println("Internet Explorer??");
+                break;
+            default:
+                System.out.println("Je gebruikt een niet ondersteunde browser");
+        }
+
 
     }
-
-    private static int fibonacci(int getal) {
-        return getal == 0 ? 0 : ((getal == 1) ? 1 : fibonacci(getal - 1 ) + fibonacci(getal -2));
-    }
-
-    private static int fibonacci2(int getal) {
-        return getal < 2 ? getal : fibonacci2(getal - 1) + fibonacci2(getal - 2);
-    }
-
-
 }
