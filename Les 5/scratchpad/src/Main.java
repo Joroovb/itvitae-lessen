854ea13c8ec9541aa526a42379abe9f522ecbf03
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-26 13:59:01 +0200

    Les 5: Break & continue

--- Les 5/scratchpad/src/Main.java
+++ Les 5/scratchpad/src/Main.java
@@ -4,39 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int[] getallen = {1, 6, 4, 1, 2, 9};
 
-        // while
-        int i = 0;
-        int[] list = new int[7];
+        naam: for (int getal : getallen) {
+            if (getal == 4) {
+                break naam;
+            }
 
-        while (i < list.length) {
-            list[i] = i +2;
-            i++;
+            if (getal == 1) {
+                continue naam;
+            }
+
+            System.out.println(getal);
         }
 
-        //do while
-        i = 0;
-        list = new int[7];
 
-        do {
-            list[i] = i + 2;
-            i++;
-        } while (i < list.length);
-
-        System.out.println(list[6]);
-
-
-        System.out.println(list[2]);
-
-        int count = 0;
-
-        do {
-
-        } while (count < 100);
-
-        while (count < 100) {
-            System.out.println(count);
-            count += 2;
-        }
     }
 }
