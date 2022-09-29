1de3961ae6abe76ad3e00bd5fca61991f543df4b
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-29 13:54:08 +0200

    Les 6: Opdracht Queue

--- Les 6/scratchpad/src/Main.java
+++ Les 6/scratchpad/src/Main.java
@@ -1,23 +1,37 @@
-import java.awt.dnd.DragGestureListener;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> getallen = new ArrayList<>();
-        getallen.add(5);
-        getallen.add(3);
-        getallen.add(1);
-        getallen.add(10);
-        getallen.add(11);
 
-        getallen.forEach(num -> System.out.println(num));
-        getallen.stream().filter(num -> num > 3);
-
-
-        for (Integer n : getallen) {
-            System.out.println(n);
-        }
 
     }
+}
+
+interface Queue {
+    // voeg een item toe aan de FIFO queue
+    public void add(int value);
+    // verwijder een item uit de FIFO queue
+    public int remove();
+    // geef het eerste item in de FIFO queue terug, maar haal het er niet uit
+    public int peek();
+    // geef aan of de FIFO queue leeg is
+    public boolean isEmpty();
+    // geef de lengte van de FIFO queue terug
+    public int size();
+    // Print de inhoud van de FIFO queue
+    public void print();
+    // verwijder alle items uit de FIFO queue
+    public void clear();
+    // verwijder de eerste n items uit de FIFO queue
+    public void clear(int n)
+    // print de inhoud van de FIFO queue in omgekeerde volgorde
+    public void printReverse();
+    // plaats een element op een bepaalde positie in de FIFO queue
+    public void jumpTheQueue(int n, int value);
+    // Zet de FIFO queue om naar een String
+    public String toString();
+    // Kijk of de FIFO queue gelijk is aan een andere FIFO queue
+    public boolean equals(Queue q);
+    // Bepaal de index van een bepaalde waarde in de FIFO queue
+    public int indexOf(int value);
+    // bepaal de laatste index van een bepaalde waarde in de FIFO queue
+    public int lastIndexOf(int value);
 }
