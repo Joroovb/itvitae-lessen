50a673f47c7d04c49d0d0c1cad748908d0bfacc2
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-29 13:49:32 +0200

    Les 6: Polymorfisme

--- Les 6/scratchpad/src/Main.java
+++ Les 6/scratchpad/src/Main.java
@@ -2,50 +2,29 @@ import java.awt.dnd.DragGestureListener;
 
 public class Main {
     public static void main(String[] args) {
-        Koffie k = new Koffie();
-        k.drinken();
-        k.vloeien();
-        Drinkbaar kk = new Koffie();
-        Drinkbaar w = new Water();
-        System.out.println(Drinkbaar.liters);
         Meeuw m = new Meeuw();
+        m.vliegen();
+        m.naam = "ja";
+        Vogel v = m;
+//        v.naam = "nee";
+        v.vliegen();
+        Meeuw mm = (Meeuw) v;
+        mm.naam = "Ja";
+        mm.vliegen();
     }
 }
 
-interface Drinkbaar {
-   int liters = 100;
-   void drinken();
-   default void vloeien() {
-       System.out.println("Ik vloei over de vloer");
-   }
-}
 
-abstract class Vogel {
-    int aantalPootjes;
-    String latijnseNaam;
-    abstract void vliegen();
+ class Vogel {
+    void vliegen() {
+        System.out.println("vogels kunnen vliegen");
+    }
 }
 
 class Meeuw extends Vogel {
+    String naam = "";
     void vliegen() {
-        
-    }
-}
-
-class Koffie implements Drinkbaar {
-    public void drinken() {
-        System.out.println("Lekker koffie drinken");
-        System.out.println("Je drinkt " + liters + " liter koffie! Lekker aan het werk!");
-    }
-
-    public void vloeien() {
-        System.out.println("Koffie op de vloer");
-    }
-}
-
-class Water implements Drinkbaar {
-    public void drinken() {
-        System.out.println("Lekker water drinken");
+        System.out.println("Meeuwen ook");
     }
 }
 
