85cf31b905ea7edfb587ddcbb937b9608635f269
Joroovb <jorisvanbreugel@mailbox.org>
2022-09-29 11:18:06 +0200

    Les 6: Overrides

--- Les 6/scratchpad/src/Main.java
+++ Les 6/scratchpad/src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Kip kip = new Kip();
+        kip.naam = "Joke";
+        kip.tokken();
+//        Vogel v = new Vogel(); // geen instantie van abstracte klasse
+//        v.vliegen();
+//        System.out.println(v);
+        kip.vliegen();
+        System.out.println(kip);
+    }
+
+}
+
+abstract class Vogel {
+    int aantalVleugels;
+    String snavelVorm;
+    String naam;
+
+    abstract void vliegen();
+
+    @Override
+    public String toString() {
+        return "Ik ben een vogel";
+    }
+}
+
+class Kip extends Vogel {
+
+    public Kip() {
+
+    }
+    
+    void tokken() {
+        System.out.println("Tok tok tok");
+    }
+
+    void vliegen() {
+        System.out.println("Ik kan niet vliegen");
+    }
+
+    @Override
+    public String toString() {
+        return "Ik ben een kip";
+    }
+
+    public String toString2() {
+        return "Ik ben een kip2";
     }
 }
