47d0d38f8863a23da7932f925519b13e4adc9b9a
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-03 11:01:46 +0200

    Les 7: Initial commit

---
+++ Les 7/scratchpad/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) {
+//        try {
+//            FileInputStream fs = new FileInputStream("Test.txt");
+//            System.out.println("Ik kan er vanalles bij zetten");
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Bestand niet gevonden");
+//            e.printStackTrace();
+//        }
+
+
+//        try {
+//            FileInputStream fs = new FileInputStream("Test.txt");
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+        throw new RuntimeException();
+    }
+}
+
+
