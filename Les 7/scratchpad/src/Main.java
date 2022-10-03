8fc87e0ac2aa7c16a7a528da84b34bb137493016
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-03 13:22:13 +0200

    Les 7: Init blocks

--- Les 7/scratchpad/src/Main.java
+++ Les 7/scratchpad/src/Main.java
@@ -1,65 +1,74 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            zonnen();
-        } catch (TeWeinigZonException e) {
-            System.out.println("Probleem met zonnen");
-            System.out.println(e);
-        } catch (Exception e) {
-            System.out.println("Een ander probleem");
-            e.printStackTrace();
-        } finally {
-            System.out.println("Hoe dan ook een mooie dag");
-        }
+//        System.out.println(Math.sqrt(-1));
+//
+//        int uitkomst = 15 / 0;
+//        System.out.println(uitkomst);
+
+//        int a = Integer.parseInt("-1");
+//        System.out.println(a);
+//        ArrayList<String> aa = new ArrayList<>();
+//        aa.get(1);
+
+        Meeuw m = new Meeuw();
+        Vogel v = new Vogel();
+        Persoon p = new Persoon();
+
+//        if (v instanceof Vogel) {
+//            System.out.println("Ik ben een vogel");
+//        }
+
+//        System.out.println(77.0 / 0);
+//        System.out.println(77.0 / 0.0);
+//        System.out.println(0.0 / 0);
+
+//        System.out.println(-1 / 0.0);
+
+//        double infin = 1.0 / 0;
+//        System.out.println(infin);
+//
+//        double dubbelinfin = infin / 0;
+//        System.out.println(dubbelinfin);
+//
+//        System.out.println(10 / infin);
+
+//        System.out.println((double) 100 / 0);
+
+//        int abc = Integer.parseInt("123_45");
+//        int abc = 123456789;
+//        System.out.println(abc);
+
+        new Main().nogEenKeer();
     }
 
-    public static void zonnen() throws TeWeinigZonException {
-        boolean genoegZon = false;
-
-        if (!genoegZon) {
-            throw new TeWeinigZonException();
-        }
-
-        System.out.println("Lekker in het zonnetje liggen");
+    void nogEenKeer() {
+        System.out.println("Nog een keer!");
+        nogEenKeer();
     }
 }
 
-class TeWeinigZonException extends Exception {
-    public TeWeinigZonException() {
-        super("De zon mag wel weer gaan schijnen");
+class Vogel {
+    {
+        System.out.println("ja hoor");
+    }
+
+    static {
+        System.out.println("Statisch");
+    }
+
+    Vogel() {
+        System.out.println("In constructor");
     }
 }
 
-class ThrowExceptions {
-    void met1() throws Error {}
-    void met2() throws Exception {}
-    void met5() throws FileNotFoundException {}
+class Meeuw extends Vogel {
 
-    void met6() {
-        try {
-             met5();
-        }
-        catch (FileNotFoundException e) {
+}
 
-        }
-    }
-
-    void met3() {
-        try { }
-        catch (Error e) { }
-    }
-
-    void met4() {
-        try {
-
-        }
-        catch (Exception e) {
-
-        }
-    }
+class Persoon {
 
 }
