7e51b213ecfa4263935eb8c46ea6d8160b0d9095
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-03 11:44:11 +0200

    Les 7: Exceptions

--- Les 7/scratchpad/src/Main.java
+++ Les 7/scratchpad/src/Main.java
@@ -4,22 +4,62 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-//        try {
-//            FileInputStream fs = new FileInputStream("Test.txt");
-//            System.out.println("Ik kan er vanalles bij zetten");
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Bestand niet gevonden");
-//            e.printStackTrace();
-//        }
+        try {
+            zonnen();
+        } catch (TeWeinigZonException e) {
+            System.out.println("Probleem met zonnen");
+            System.out.println(e);
+        } catch (Exception e) {
+            System.out.println("Een ander probleem");
+            e.printStackTrace();
+        } finally {
+            System.out.println("Hoe dan ook een mooie dag");
+        }
+    }
 
+    public static void zonnen() throws TeWeinigZonException {
+        boolean genoegZon = false;
 
-//        try {
-//            FileInputStream fs = new FileInputStream("Test.txt");
-//        } catch (FileNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
-        throw new RuntimeException();
+        if (!genoegZon) {
+            throw new TeWeinigZonException();
+        }
+
+        System.out.println("Lekker in het zonnetje liggen");
     }
 }
 
+class TeWeinigZonException extends Exception {
+    public TeWeinigZonException() {
+        super("De zon mag wel weer gaan schijnen");
+    }
+}
 
+class ThrowExceptions {
+    void met1() throws Error {}
+    void met2() throws Exception {}
+    void met5() throws FileNotFoundException {}
+
+    void met6() {
+        try {
+             met5();
+        }
+        catch (FileNotFoundException e) {
+
+        }
+    }
+
+    void met3() {
+        try { }
+        catch (Error e) { }
+    }
+
+    void met4() {
+        try {
+
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+}
