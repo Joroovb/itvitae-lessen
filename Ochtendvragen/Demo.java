fbba1b89c5c3719992e4de94a6c78f340d485a64
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-06 10:10:53 +0200

    OV: SA

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,5 +1,16 @@
 class Demo { // RK
 	public static void main(String... ab) {
-	
+		Kip kap = new Kip();
+		kap.herewego();
+		System.out.println("tok");
+	}
+}
+
+class Kip {
+	boolean boos;
+
+	static int herewego() {
+		System.out.println("Is de kip boos?" + boos);
+		return 9;
 	}
 }
