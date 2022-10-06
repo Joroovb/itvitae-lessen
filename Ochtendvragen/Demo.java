1a42b416208daec9d0488eb0d897c2b702b09bd5
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-06 10:31:42 +0200

    OV: SH

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,9 +1,11 @@
-class Demo { // SF
+class Demo { // SG
 	public static void main(String... ab) {
 		Kip kap = new Kip();
 	
 		try {
 			kap.herewego();
+		} catch (Exception e) {
+			System.out.println("Ik ben een fout");
 		} catch (NoEggsException e) {
 			System.out.println("Gaat fout");
 		} finally {
