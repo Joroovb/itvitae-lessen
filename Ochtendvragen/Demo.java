572c60c9d3ebb2e92915c2fd183f4c1de25f4fcd
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-10 09:52:29 +0200

    OV: leeg

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,34 +1,4 @@
-class Demo { // AB
-	public static void main(String... ab) {
-		Q q = new E(); 
-// Links bepaald waar je bij kan
-// Rechts bepaald wat je doet
-		L l = new E();
-		E e = new E();
-		l.i();
-
-		// Doosje	// Wat er in moet
-		// Gat		// blokje
-		// Generiek // specifiek
-		Meeuw m = new Meeuw();
-		Dier d = new Meeuw();
-		Dier dd = new Dier();
-		Meeuw mm = new Dier();	
+class Demo{   // LEEG
+	public static void main(String... ab){
 	}
 }
-
-class Q {}
-
-class E extends Q implements L {
-	public void i() {
-		System.out.println("In E de I");
-	}
-}
-
-interface L {
-	void i();
-}
-
-class Dier {}
-
-class Meeuw extends Dier {}
