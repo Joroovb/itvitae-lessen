383600224cbe9c6e70d70acc2d88dcd35aa53a6f
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-10 10:12:52 +0200

    OV: NC

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,4 +1,4 @@
-class Demo{   // NB
+class Demo{   // NC
 	public static void main(String... ab){
 		System.out.println("Let's go!");
 		new Demo().beginnen();
@@ -9,9 +9,16 @@ class Demo{   // NB
 		Go so = new Go();
 		no.a = 15;
 		System.out.println(so.a);
+		no.hoi();
 	}
 }
 
 class Go {
 	static int a = 5;
+	
+	void hoi() {
+		int a = 7;
+		System.out.println(a);
+		a = a;
+	}
 }
