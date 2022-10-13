1bf22af93d2d1e79c596743b07bdf24e3b38feaf
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-13 10:19:30 +0200

    OV: NQ

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,29 +1,7 @@
-class Demo{   // NP
+class Demo{   // NQ 
+	//java Demo a b c d e
 	public static void main(String... ab) {
-		System.out.println("oei!");
-		Andere dezelfde = new Andere();
-		try {
-			dezelfde.hup();
-		} catch (Throwable e) {
-			System.out.println("Gaat fout!");
-		} finally {
-			System.out.println("Gered!");
-		}
-		System.out.println("Einde");
+		System.out.println("OK" + ab.length);
 	}
 }
 
-class Andere implements H {
-	void hup() {
-		throw new Error();
-	}
-
-	Object gaan() {
-		H ah = new Andere();
-		return ah;
-	}
-}
-
-interface H {
-
-}
