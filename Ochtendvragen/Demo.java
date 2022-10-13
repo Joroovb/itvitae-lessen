a6e715f9b109267bd12ec5355b31d0630b0ffe36
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-13 10:27:32 +0200

    OV: NT

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,8 +1,12 @@
-class Demo{   // NS
+class Demo{   // NT
 	//java Demo a b c d e
-	final static public int main(String[] ab) {
+	final static public void main(String[] ab) {
 		System.out.println("OK" + ab[3]);
-		return 3;
+		oei();
+	}
+
+	void oei(int... daargaanwe) {
+		System.out.println(daargaanwe[daargaanwe.length - 1]);
 	}
 }
 
