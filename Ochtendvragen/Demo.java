05038b0a0d4f262d73d1d88cbb1cb0814b0f41a4
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-13 10:13:41 +0200

    OV: NO

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,19 +1,28 @@
-class Demo{   // NM
+class Demo{   // NO
 	public static void main(String... ab) {
-		// throw new Error();
 		System.out.println("oei!");
 		Andere dezelfde = new Andere();
 		try {
 			dezelfde.hup();
 		} catch (Throwable e) {
 			System.out.println("Gaat fout!");
+		} finally {
+			System.out.println("Gered!");
 		}
 		System.out.println("Einde");
 	}
 }
 
-class Andere {
+class Andere implements H {
 	void hup() {
 		throw new Error();
 	}
+
+	Object gaan() {
+		return new H();
+	}
+}
+
+interface H {
+
 }
