f7dba81b2be33ffbb999a4a34d2104e51eb7b93a
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-17 10:06:36 +0200

    OV: OH

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,10 +1,12 @@
-class Demo{   // OG
+class Demo{   // OH
 	final static public void main(String[] ab) {
 		HereWeGo go = new HereWeGo();
 		go.gaan();
 	}
 }
 
-class HereWeGo {
+abstract class HereWeGo {
+// Als je een abstracte methode hebt, 
+//moet de class ook abstract zijn
 	abstract void gaan();
 }
