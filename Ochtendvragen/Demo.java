e5309247d31d1f733089bdf1516e82ad4831e822
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-17 10:05:16 +0200

    OV: OG

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,33 +1,10 @@
-class Demo{   // OD
+class Demo{   // OG
 	final static public void main(String[] ab) {
-
-		// Links bepaald waar we bij kunnen
-		// Rechts bepaald wat we doen
-		
-		B c = new B();
-		c.gaan();
-		System.out.println("Alles goed gegaan");
+		HereWeGo go = new HereWeGo();
+		go.gaan();
 	}
 }
 
-class A implements C {
-	public B gaan() {
-		System.out.println("Gaan in a!");
-		return new B();
-	}
-}
-
-//Override of overload?
-
-class B extends A {
-	public B gaan() {
-		System.out.println("Gaan in b!");
-		return null;
-	}
-
-}
-
-interface C {
-	int d = 5;
-	B gaan() throws Exception;
+class HereWeGo {
+	abstract void gaan();
 }
