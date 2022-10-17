c98d1db13131e87074a35106a693b7b5b6ad3afd
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-17 10:27:41 +0200

    OV: OM

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,40 +1,42 @@
-class Demo{   //OL
+class Demo{   //OM
 	final static public void main(String[] ab) {
-		// We mogen geen instantie maken van
-		// abstracte class
 		HereWeGo go = new Rechthoek();
 		go.hup();
 		go.gaan();
-
-		Rond r = new Rond();
+		System.out.println(go.a);
+		Rond r = go;
 		r.gaan();
+		r.hup();
 	}
 }
 
 abstract class HereWeGo {
-// Als je een abstracte methode hebt, 
-//moet de class ook abstract zijn
+	int a = 3;
 	abstract void gaan();
-
 	static void hup() {
 		System.out.println("We gaan!");
 	}
 }
 
 class Rond extends HereWeGo {
+	int a = 4;
 	void gaan() {
 		System.out.println("Ronde wielen aan de bus");
 	}
 }
 
 abstract class Vierkant extends Rond {
-
+	int a = 4;
+	abstract void gaan();
 }
 
 class Rechthoek extends Vierkant {
+	int a = 6;
+	void gaan() {
+		System.out.println("We zijn in vierkant");
+	}
 
 	static void hup() {
 		System.out.println("Rechthoek formatie!");
 	}
-
 }
