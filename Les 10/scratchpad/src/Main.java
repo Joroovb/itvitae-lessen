e4a999492595cae2da8da0797f88f235e7c25b59
Joroovb <jorisvanbreugel@mailbox.org>
2022-10-31 11:52:46 +0100

    Les 10: Garbage collector

--- Les 10/scratchpad/src/Main.java
+++ Les 10/scratchpad/src/Main.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        // Garbage collector
+        // Eligible for garbage collection
+        int a = 8;
+        Vogel V = new Vogel("Pieter de vogel");
+        Vogel V2 = new Vogel("Jan de Vogel");
+        V.eten(V2);
+        V.aantalFladderen(a);
+        System.out.println(a);
+        System.out.println(V2.naam);
+    }
 }
+
+class Vogel {
+    String naam;
+
+    Vogel(String naam) {
+        this.naam = naam;
+    }
+
+    void aantalFladderen(int fladders) {
+        for(int i = 0; i < fladders; ++i){
+            System.out.println("Fladder");
+        }
+        fladders = 9;
+    }
+
+    void eten(Vogel v) {
+        System.out.println("Lekker " + v.naam + " eten");
+        v.naam = "Bob de vogel";
+    }
+}
+
