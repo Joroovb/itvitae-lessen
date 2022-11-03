e1b009dac535c264367c36c165a9544331e78720
Joroovb <jorisvanbreugel@mailbox.org>
2022-11-03 10:47:02 +0100

    OV: WCC

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,14 +1,14 @@
-class Demo{   // WA
+class Demo{   // WCC
 	final static public void main(String[] ab){ 
-		System.out.println(0b10);
+		System.out.println(0x2C);
 	}
 }
 
 
-// 0 1 2 3 4 5 6 7 - 8 prefix = 01111
-// 0 1 - 2 prefix = 0b0000
-// 0 1 2 3 4 5 6 7 8 9 - 10	prefix = null
-// 0 .. 9 A .. F - 16 prefix = 0x0000
+// 0 1 2 3 4 5 6 7 - 8 prefix = 01111 octal
+// 0 1 - 2 prefix = 0b0000 binair
+// 0 1 2 3 4 5 6 7 8 9 - 10	prefix = null decimal
+// 0 .. 9 A .. F - 16 prefix = 0x0000 hexadecimal
 
 //			8	1
 //			1	2 = 10
