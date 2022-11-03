fdabdd04a5997a3ef5cca1c405308634931cd1f9
Joroovb <jorisvanbreugel@mailbox.org>
2022-11-03 10:42:51 +0100

    OV: WA 2

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,5 +1,19 @@
 class Demo{   // WA
 	final static public void main(String[] ab){ 
-		System.out.println(010);
+		System.out.println(0b10);
 	}
 }
+
+
+// 0 1 2 3 4 5 6 7 - 8 prefix = 01111
+// 0 1 - 2 prefix = 0b0000
+// 0 1 2 3 4 5 6 7 8 9 - 10	prefix = null
+// 0 .. 9 A .. F - 16 prefix = 0x0000
+
+//			8	1
+//			1	2 = 10
+
+//			2	1
+//			0   1 - 1
+//			1   0 - 2
+//			1   1 - 3
