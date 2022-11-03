fae70041831891a1253a39ef45b8f1259b70de5c
Joroovb <jorisvanbreugel@mailbox.org>
2022-11-03 10:23:46 +0100

    OV: QD

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,10 +1,27 @@
-class Demo{   // QC
+class Demo{   // QD
 	final static public void main(String[] ab){ 
-		int a = 'e';
-		char c = (char) a;
-		// unboxing
-		long d = new Long(a);
-		float e = 25.3;
-		System.out.println(d);
+		new WWW().worldWideWebben();
+		System.out.println("We gaan door!");
 	}
 }
+
+class WWW {
+	void worldWideWebben() {
+		try {
+			new TTT.proberen();
+		} catch (Exception e) {
+			System.out.println("Gevangen!");
+		} finally {
+			return;
+		}
+		System.out.println("Ik leef nog!");
+	}
+}
+
+class TTT {
+	void proberen() {
+		throw new HereWeGoException();
+	}
+}
+
+class HereWeGoException extends Error{}
