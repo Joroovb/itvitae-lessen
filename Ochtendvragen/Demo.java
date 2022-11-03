260b48a9021192e6bd2a65a1078ca3e921a27307
Joroovb <jorisvanbreugel@mailbox.org>
2022-11-03 10:15:03 +0100

    OV: PZ 2

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -2,7 +2,13 @@ class Demo{   // PZ
 	final static public void main(String[] ab){ 
 		int a = 'e';
 		char c = (char) a;
-		long d = new Integer("2");
-		System.out.println(d);
+		// unboxing
+		long d = new Integer("2"); // wrapper
+
+		// auto-boxing
+		Integer aa = 1;
+		Integer aaa = null;
+
+		System.out.println(aa);
 	}
 }
