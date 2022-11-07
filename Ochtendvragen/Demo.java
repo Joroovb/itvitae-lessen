59b3d3497f5cec9dda1d89136f13a63f14a44372
Joroovb <jorisvanbreugel@mailbox.org>
2022-11-07 10:29:08 +0100

    OV: WO

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,39 +1,22 @@
-class Demo{   //  WM
+class Demo{   //  WO
 	final static public void main(String[] ab){
-		// Links bepaald waar je bij kunt
-		// Rechts bepaald wat je kunt
-		WWB vb = new VB();
-		vb.gaan(false);
-		System.out.println(vb.a);
-		System.out.println("einde");
-	}
-	static void lopen(double v){
-		System.out.println("lopen1" + v);
-	}
-	static void lopen(int a){
-		System.out.println("lopen2" + a);		
-	}
-}
-class WWB {
-	int a = gaan(true);
-	int gaan(boolean g) {
-		return 24;
+		Fiets fiets = new Fiets();
+		fiets.fietsen();
 	}
 }
 
-class VB extends WWB{
-	int a = gaan(false);
-	int gaan(boolean t){
-		if(t){
-			System.out.println("lopen3");
-			return 7;
-		}else{
-			System.out.println("lopen5");
-		}
-		return 9;
+class Fiets {
+	Bel bel;
+
+	void fietsen() {
+		bel.bellen();
+		System.out.print("Ik fiets!");
 	}
-	float gaan(char w){
-		System.out.println("lopen4");
-		return 6.3f;
+}
+
+class Bel {
+	void bellen() {
+		System.out.print("Ik ben aan het bellen");
+		throw new RuntimeException();
 	}
 }
