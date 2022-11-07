fdc046772c723de343abc1ca897ffcdde250c1d4
Joroovb <jorisvanbreugel@mailbox.org>
2022-11-07 10:47:03 +0100

    OV: XA

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,33 +1,9 @@
-class Demo{   //  WZ
+class Demo{   //  XA
 	final static public void main(String[] ab){
-		Fiets fiets = new Fiets();
-		try {
-			fiets.fietsen();			
-		} catch (Exception bk) {
-			System.out.println("Bel heel erg kapot");
-		} catch (BelKapotException e) {
-			System.out.println("bel kapot");
-		} finally {
-			System.out.println("a");
-		}
-		System.out.println("b");
+		int a = 34;
+		char b = (char) a;
+		b++;
+		System.out.println(a);
+		System.out.println("Einde");
 	}
 }
-
-class Fiets {
-	Bel bel = new Bel();
-
-	void fietsen() throws BelKapotException {
-		bel.bellen();
-		System.out.println("Ik fiets!");
-	}
-}
-
-class Bel {
-	void bellen() throws BelKapotException{
-		System.out.println("Ik ben aan het bellen");
-		throw new BelKapotException();
-	}
-}
-
-class BelKapotException extends Exception {}
