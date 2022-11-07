92954231838160d0b7149b13868ff537bb706d48
Joroovb <jorisvanbreugel@mailbox.org>
2022-11-07 10:07:06 +0100

    OV: WH

--- Ochtendvragen/Demo.java
+++ Ochtendvragen/Demo.java
@@ -1,18 +1,19 @@
-class Demo{   //  WG
+class Demo{   //  WH
 	final static public void main(String[] ab){
 		lopen(new VB().gaan(true));
 		System.out.println("einde");
 	}
 	static void lopen(double v){
-		System.out.println("lopen1");
+		System.out.println("lopen1" + v);
 	}
 	static void lopen(int a){
-		System.out.println("lopen2");		
+		System.out.println("lopen2" + a);		
 	}
 }
 
 class VB{
 	int a = gaan(false);
+
 	int gaan(boolean t){
 		if(t){
 			System.out.println("lopen3");
@@ -20,7 +21,9 @@ class VB{
 		}else{
 			System.out.println("lopen5");
 		}
+		return 9;
 	}
+
 	float gaan(char w){
 		System.out.println("lopen4");
 		return 6.3f;
